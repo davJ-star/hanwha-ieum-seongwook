d61193fb0fff068e46bfbb8445658391832e647f
zxfg223 <zxfg223@gmail.com>
2025-02-01 00:25:34 +0900

    Initial backend commit

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,17 +1,33 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Normalized;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 @RequiredArgsConstructor
+@Transactional
 public class TestController {
+    private final UserRepository userRepository;
+
     @GetMapping("/test")
     @ResponseBody
     public String test(){
         return "hello,world!";
+    }
+
+    @GetMapping("/db_test")
+    @ResponseBody
+    public String dbtest(){
+        Optional<User> user = userRepository.findById(1);
+        return user.get().getEmail();
     }
 }
